16ae5521ee068f08c5900d1847644af7ab927e1c
Arpan Adhikari <adhikariarpan2063@gmail.com>
2026-07-31 11:40:03 +0545

    Update in eslint
    * Added eslint script to fix error as lint:fix
    * Updated eslint to include tailwind plugin

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,20 +2,38 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    ignores: ['vite.config.js'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: { tailwindcss },
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      tailwindcss: {
+        config: 'tailwind.config.js',
+      },
+    },
+    rules: {
+      'tailwindcss/enforces-shorthand': 'error',
+      'tailwindcss/classnames-order': 'warn',
+    },
+  },
+  {
+    files: ['vite.config.js'],
+    languageOptions: {
+      globals: globals.node,
     },
   },
 ])
