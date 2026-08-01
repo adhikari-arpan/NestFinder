24f4169efefe437509874f7bf4f696efeef87405
Arpan Adhikari <adhikariarpan2063@gmail.com>
2026-08-02 00:08:55 +0545

    Nepal provinces data

---
+++ client/src/components/kyc/nepalProvinces.js
@@ -0,0 +1,9 @@
+export const NEPAL_PROVINCES = [
+  'Koshi Province',
+  'Madhesh Province',
+  'Bagmati Province',
+  'Gandaki Province',
+  'Lumbini Province',
+  'Karnali Province',
+  'Sudurpashchim Province',
+];
