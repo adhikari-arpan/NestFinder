2dcb2073c1df1fea75e85d2cf9e650d7b24ad555
Arpan Adhikari <adhikariarpan2063@gmail.com>
2026-08-02 00:18:10 +0545

    Update nepal provinces data with nepal locations for all levels

--- client/src/components/kyc/nepalProvinces.js
+++
@@ -1,9 +0,0 @@
-export const NEPAL_PROVINCES = [
-  'Koshi Province',
-  'Madhesh Province',
-  'Bagmati Province',
-  'Gandaki Province',
-  'Lumbini Province',
-  'Karnali Province',
-  'Sudurpashchim Province',
-];
